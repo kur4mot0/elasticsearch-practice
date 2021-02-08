a76a789df27954363f40cc65a048523a730299a0
pkr <pkr@elasticsearch.kura.infra>
2021-02-08 21:04:30 +0100

    Create, Get and Delete index

--- session1/index.js
+++ session1/index.js
@@ -1,14 +1,73 @@
 const inquirer = require('inquirer');
 
+function CreateIndex() {
+   inquirer
+   .prompt([
+      {
+         type: "input",
+         name: "indexName",
+         message: "Please provide an index name: "         
+      }
+   ])
+   .then(function (answer)
+   {
+      console.log(answer);
+   })
+   .catch()
+}
+
+function GetIndex() {
+   inquirer
+   .prompt([
+      {
+         type: "input",
+         name: "indexName",
+         message: "Please provide an index name: "         
+      }
+   ])
+   .then(function (answer)
+   {
+      console.log(answer);
+   })
+   .catch()
+}
+
+function DeleteIndex() {
+   inquirer
+   .prompt([
+      {
+         type: "input",
+         name: "indexName",
+         message: "Please provide an index name: "         
+      }
+   ])
+   .then(function (answer)
+   {
+      console.log(answer);
+   })
+   .catch()
+}
+
 function run(){
-   inquirer.prompt({
+   inquirer
+   .prompt({
       type: 'rawlist',
       name: "action",
       message: "Action?",
       choices: ["Create Index", "Get Index", "Delete Index"]
    })
    .then(function (answers) {
-      console.log(answers);
+      switch (answers['action']) {
+         case "Create Index":
+            CreateIndex();
+            break;
+         case "Get Index":
+            GetIndex();
+            break;
+         case "Delete Index":
+            DeleteIndex();
+            break;         
+      }
    })
    .catch()
 }
